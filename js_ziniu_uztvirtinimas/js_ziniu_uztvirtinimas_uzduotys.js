67bed99f1473fe16232a73ad0f49521b5dd299b0
Paulius <paulius000@gmail.com>
2024-01-12 14:56:23 +0200

    renaming directories to match the lessons

---
+++ js_ziniu_uztvirtinimas/js_ziniu_uztvirtinimas_uzduotys.js
@@ -0,0 +1,13 @@
+// 1.
+console.log("1. Užduotis:");
+
+let euroDollarRate = 1.1;
+
+const euroToDollar = (euro) => euro * euroDollarRate;
+
+console.log(`Penki eurai doleriais: ${euroToDollar(5)}`);
+
+// console.log("===========================\n");
+
+// 2.
+console.log("2. Užduotis:");
